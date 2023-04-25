84b2c73f5109f0eaaeaff867a45bdd2421725c1b
@you._.jin_2 <86221508+youjin2github@users.noreply.github.com>
2023-04-25 23:08:30 +0900

    Add files via upload

---
+++ 추억점수.py
@@ -0,0 +1,10 @@
+def solution(name, yearning, photo):
+    answer = []
+    for num in range(len(photo)):
+        score = 0
+        for i in range(len(photo[num])):
+            for j in range(len(name)):
+                if name[j] == photo[num][i]:
+                    score += yearning[j]
+        answer.append(score)
+    return answer
